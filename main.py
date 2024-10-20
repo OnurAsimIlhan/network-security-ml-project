21950b8c4b8cb23d649d4b5691d1d405e664423d
Onur Asim Ilhan <asim.ilhan@ug.bilkent.edu.tr>
2024-10-20 13:42:40 +0300

    data validation steps added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from networksecurity.componenets.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-
+from networksecurity.componenets.data_validation import DataValidation
 import sys
 if __name__=='__main__':
     try:
@@ -12,6 +12,14 @@ if __name__=='__main__':
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("Enter the try block")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
