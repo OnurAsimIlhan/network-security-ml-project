ae51a490af9b0825dc644ccf19d5c76e8625c152
Onur Asim Ilhan <asim.ilhan@ug.bilkent.edu.tr>
2024-10-20 21:40:42 +0300

    model training and experiment tracking with mlflow added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from networksecurity.componenets.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.componenets.data_validation import DataValidation
 from networksecurity.componenets.data_transformation import DataTransformation
+from networksecurity.componenets.model_trainer import ModelTrainer
 import sys
 if __name__=='__main__':
     try:
@@ -30,5 +31,11 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
